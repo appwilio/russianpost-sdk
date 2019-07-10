3d440ff7152d1865c7fd1b973859e6c39bfe6880
JhaoDa <jhaoda@gmail.com>
2019-07-11 00:43:28 +0700

    Вынос перечислений в общий слой

---
+++ src/Dispatching/Enum/MailEntryType.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Appwilio\RussianPostSDK\Dispatching\Enum;
+
+final class MailEntryType
+{
+    /**
+     * Категории вложения
+     *
+     * @see https://otpravka.pochta.ru/specification#/enums-base-entries-type
+     */
+    public const GIFT = 'GIFT';
+    public const OTHER = 'OTHER';
+    public const DOCUMENT = 'DOCUMENT';
+    public const SALE_OF_GOODS = 'SALE_OF_GOODS';
+    public const COMMERCIAL_SAMPLE = 'COMMERCIAL_SAMPLE';
+}
---
+++ src/Dispatching/Enum/PaymentType.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Appwilio\RussianPostSDK\Dispatching\Enum;
+
+final class PaymentType
+{
+    /**
+     * Способы оплаты
+     *
+     * @see https://otpravka.pochta.ru/specification#/enums-payment-methods
+     */
+    public const STAMP = 'STAMP';
+    public const CASHLESS = 'CASHLESS';
+    public const FRANKING = 'FRANKING';
+    public const TO_FRANKING = 'TO_FRANKING';
+    public const ONLINE_PAYMENT_MARK = 'ONLINE_PAYMENT_MARK';
+}
