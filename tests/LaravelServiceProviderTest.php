c733328e5666c9d03fc875e841dd157f48ce514b
JhaoDa <jhaoda@gmail.com>
2019-09-19 01:55:48 +0700

    Тест сервис-провайдера Laravel

---
+++ tests/LaravelServiceProviderTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of RussianPost SDK package.
+ *
+ * © Appwilio (http://appwilio.com), greabock (https://github.com/greabock), JhaoDa (https://github.com/jhaoda)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Appwilio\RussianPostSDK\LaravelServiceProvider;
+use Appwilio\RussianPostSDK\Tracking\PacketAccessClient;
+use Appwilio\RussianPostSDK\Tracking\SingleAccessClient;
+use Appwilio\RussianPostSDK\Dispatching\DispatchingClient;
+use Illuminate\Contracts\Foundation\Application as ApplicationContract;
+
+class LaravelServiceProviderTest extends TestCase
+{
+    /** @var \PHPUnit\Framework\MockObject\MockObject|ApplicationContract */
+    private $app;
+
+    /** @var LaravelServiceProvider */
+    private $provider;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->app = $this->createMock(ApplicationContract::class);
+        $this->provider = new LaravelServiceProvider($this->app);
+    }
+
+    public function test_provides(): void
+    {
+        $this->assertEqualsCanonicalizing($this->provider->provides(), [
+            DispatchingClient::class,
+            PacketAccessClient::class,
+            SingleAccessClient::class,
+        ]);
+    }
+}
